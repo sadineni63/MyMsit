6c61db90672f0a4ae8ab90975f0eaf1341f772c0
udaychonu <udaykiran0528@gmail.com>
2015-04-27 15:00:28 +0530

    calendar added

--- src/main/java/net/mymsit/controllers/CalendarController.java
+++ src/main/java/net/mymsit/controllers/CalendarController.java
@@ -1,8 +1,14 @@
 package net.mymsit.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class CalendarController {
+	
+	@RequestMapping("calendar.html")
+	public String calendar() {
+		return "calendar";
+	}
 
 }
