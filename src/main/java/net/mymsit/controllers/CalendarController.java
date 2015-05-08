60bd8145f657933594bf4f0df8a81680f21a7157
udaychonu <udaykiran0528@gmail.com>
2015-05-09 03:44:32 +0530

    udating the calendar

--- src/main/java/net/mymsit/controllers/CalendarController.java
+++ src/main/java/net/mymsit/controllers/CalendarController.java
@@ -1,17 +1,27 @@
 package net.mymsit.controllers;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller 
+@Controller
 public class CalendarController {
-	
-	@RequestMapping("calendar.html")
+
+	@RequestMapping("/calendar.html")
 	public String calendar(HttpServletRequest request) {
-		return "calendar";
+		HttpSession session=request.getSession();
+		if(session.getAttribute("username")!=null)
+		{
+			System.out.println(session.getAttribute("role").toString());
+			if(session.getAttribute("role").toString().equalsIgnoreCase("student"))
+				return "student_calendar";
+			else
+				return "mentor_calendar";
+		}
+		else
+			return "signin/login";
 	}
-	
 
 }
--- src/main/webapp/WEB-INF/view/profile.jsp
+++ src/main/webapp/WEB-INF/view/profile.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-	pageEncoding="ISO-8859-1"%>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,13 +7,12 @@
 <head>
 
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">l
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
 
 <title>Profile</title>
-<title>Dashboard</title>
 
 <!-- Bootstrap Core CSS -->
 <link href="resources/css/bootstrap.min.css" rel="stylesheet">
